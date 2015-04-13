2869864add76e391e6eb6cc573d0cd65feb86124
Nicholas Botzer <nicholas.botzer@gmail.com>
2015-04-13 10:52:52 -0400

    Changed height scaling

--- listSelection.lua
+++ listSelection.lua
@@ -81,7 +81,7 @@ function scene:show( event )
         scrollView = widget.newScrollView
         {
             x = width/2,
-            y = (height*.1/2) + ((height*.9)/2),
+            y = (height/2) + (height*.1/2),
             width = width,
             height = height*.9,
             scrollWidth = width,
